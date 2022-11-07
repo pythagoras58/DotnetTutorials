e3de9dbbc26707a22b2f84eea88374bc11b9a406
SolomonDa <solomonda@stlghana.com>
2022-11-07 17:25:04 +0000

    Add project files.

---
+++ DotnetTutorials/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
